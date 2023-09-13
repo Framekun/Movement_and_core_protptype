886a7b1dc64163ae8a5a33988756f689a198d628
Nonood Whatever <70419044+NonoodWhatever@users.noreply.github.com>
2023-09-13 11:16:30 +0700

    ScoreRank Stuff
    Non-fuctional yet, technically anyway.

---
+++ Assets/Script/ScoreSystem/ScoreRankingSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreRankingSystem : MonoBehaviour
+{
+    [SerializeField] int RankARequirement;
+    [SerializeField] int RankBRequirement;
+    [SerializeField] int RankCRequirement;
+    [SerializeField] int RankDRequirement;
+    int ScoreRecieved = PlayerPrefs.GetInt("ScoreHolder");
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if(RankDRequirement > ScoreRecieved)
+        {
+
+        }
+        else if (RankDRequirement < ScoreRecieved && RankCRequirement > ScoreRecieved)
+        {
+
+        }
+        else if (RankCRequirement < ScoreRecieved && RankBRequirement > ScoreRecieved)
+        {
+
+        }
+        else if (RankBRequirement < ScoreRecieved && RankARequirement > ScoreRecieved)
+        {
+
+        }
+        else if (RankARequirement < ScoreRecieved)
+        {
+
+        }
+        else
+        {
+            print("HEY YOU FORGOT TO INSERT SCORES");
+        }
+    }
+}
