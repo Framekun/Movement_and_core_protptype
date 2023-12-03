93da03ec26fffc6772f1ce4f2f7f2a010e26ef21
Nonood Whatever <70419044+NonoodWhatever@users.noreply.github.com>
2023-12-03 21:57:43 +0700

    GameClearGameOver Visual changes + RNG End Screen Code.

---
+++ Assets/Asset/Code/GameClearGameFailRNG.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class GameClearGameFailRNG : MonoBehaviour
+{
+    [SerializeField] private GameObject[] NumberStuff;
+    private int LuckyNumber;
+    private GameObject LuckyObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        LuckyNumber = Random.Range(0, NumberStuff.Length-1);
+        Debug.Log(LuckyNumber);
+        Debug.Log(NumberStuff.Length);
+        for (int i = 0; i < NumberStuff.Length; i++)
+        {
+            if (i == LuckyNumber)
+            {
+                NumberStuff[i].SetActive(true);
+            }
+            else
+            {
+                NumberStuff[i].SetActive(false);
+            }
+        }
+   
+    }
+
+   
+}
